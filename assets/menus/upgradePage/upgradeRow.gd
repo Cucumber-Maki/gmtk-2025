23fa873cb5014c771d64b04cbe7c611e185f1ec1
danielday-dev <141579840+danielday-dev@users.noreply.github.com>
2025-08-03 11:49:01 +0100

    add second category to upgradeRow Exports

--- assets/menus/upgradePage/upgradeRow.gd
+++ assets/menus/upgradePage/upgradeRow.gd
@@ -4,8 +4,10 @@ enum UpgradeResource{
 	Nubbin,
 	Niblet,
 }
-@export_category("Upgrade Details")
+@export_category("Upgrade Information")
 @export var buttonName : String = "Name Goes Here";
 @export var buttonHoverDescription : String = "Description";
-@export var requiredResource : UpgradeResource;
+
+@export_category("Upgrade Values")
 @export var costMultiplier : float = 1.07;
+@export var requiredResource : UpgradeResource;
